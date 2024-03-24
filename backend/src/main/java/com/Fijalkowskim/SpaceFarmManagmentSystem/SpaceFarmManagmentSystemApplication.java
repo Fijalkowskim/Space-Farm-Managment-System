16d248018dc6ecae0e15d77af127706727b03347
Mateusz <fijalkowskim01@gmail.com>
2024-03-24 19:15:28 +0100

    Backend project initialised

---
+++ backend/src/main/java/com/Fijalkowskim/SpaceFarmManagmentSystem/SpaceFarmManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Fijalkowskim.SpaceFarmManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFarmManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceFarmManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Fijalkowskim/SpaceFarmManagmentSystem/SpaceFarmManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fijalkowskim.SpaceFarmManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceFarmManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
