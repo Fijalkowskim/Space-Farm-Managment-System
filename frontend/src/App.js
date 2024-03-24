768d839ec2cb5241148fd45f84d5b19bc2519d29
Mateusz <fijalkowskim01@gmail.com>
2024-03-24 19:27:57 +0100

    Frontend project initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-5xl">
+      Space Farm Managment System
+    </div>
+  );
+}
+
+export default App;
