eb189f39113b1f95727fd3bc64e383e3ea5a0074
Mateusz <fijalkowskim01@gmail.com>
2024-04-27 19:23:31 +0200

    Loading cultivation details

--- frontend/src/pages/detailsPages/CultivationDetails.jsx
+++ frontend/src/pages/detailsPages/CultivationDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageWrapper from "../PageWrapper";
 import { useParams } from "react-router-dom";
 import { useCultivationContext } from "../../context/CultivationContext";
@@ -6,8 +6,30 @@ import { useCultivationContext } from "../../context/CultivationContext";
 function CultivationDetails() {
   const { id } = useParams();
   const { getCultivation } = useCultivationContext();
-  cons;
-  return <PageWrapper secured={true}>{id}</PageWrapper>;
+  const [cultivation, setCultivation] = useState(undefined);
+
+  useEffect(() => {
+    const loadCultivation = async () => {
+      try {
+        const cultivationId = parseInt(id);
+        const data = await getCultivation(cultivationId);
+        setCultivation(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadCultivation();
+  }, [getCultivation, setCultivation, id]);
+
+  return (
+    <PageWrapper secured={true}>
+      {cultivation === undefined ? (
+        <h1>There is no cultivation with given id.</h1>
+      ) : (
+        <>Cultivation {id}</>
+      )}
+    </PageWrapper>
+  );
 }
 
 export default CultivationDetails;
