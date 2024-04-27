5562223e8818bdf7b94fa01819e3d5822b7832f7
Mateusz <fijalkowskim01@gmail.com>
2024-04-27 22:10:18 +0200

    useCultivation custom hook for loading cultivation details

---
+++ frontend/src/hooks/useCultivation.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useCultivationContext } from "../context/CultivationContext";
+export const useCultivation = (id) => {
+  const [cultivation, setCultivation] = useState();
+  const [isPending, setIsPending] = useState(false);
+  const { getCultivation } = useCultivationContext();
+
+  useEffect(() => {
+    const loadCultivation = async () => {
+      setIsPending(true);
+      try {
+        const cultivationId = parseInt(id);
+        const data = await getCultivation(cultivationId);
+        setCultivation(data);
+      } catch (err) {
+        console.log(err);
+      }
+      setIsPending(false);
+    };
+    loadCultivation();
+  }, [getCultivation, setCultivation, id, setIsPending]);
+
+  return { cultivation, isPending };
+};
--- frontend/src/pages/detailsPages/CultivationDetails.jsx
+++ frontend/src/pages/detailsPages/CultivationDetails.jsx
@@ -1,36 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PageWrapper from "../PageWrapper";
 import { useParams } from "react-router-dom";
-import { useCultivationContext } from "../../context/CultivationContext";
+import { useCultivation } from "../../hooks/useCultivation";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function CultivationDetails() {
   const { id } = useParams();
-  const { getCultivation } = useCultivationContext();
-  const [cultivation, setCultivation] = useState(undefined);
-
-  useEffect(() => {
-    const loadCultivation = async () => {
-      try {
-        const cultivationId = parseInt(id);
-        const data = await getCultivation(cultivationId);
-        setCultivation(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    loadCultivation();
-  }, [getCultivation, setCultivation, id]);
-
+  const { cultivation, isPending } = useCultivation(id);
   return (
-    <PageWrapper secured={true}>
-      {cultivation === undefined ? (
+    <PageWrapper secured={true} className={"h-fit min-h-0"}>
+      {cultivation === undefined && !isPending ? (
         <div className="h-screen flex items-center justify-center">
           <h1 className="text-xl text-center">
             There is no cultivation with given id.
           </h1>
         </div>
+      ) : isPending ? (
+        <div className="animate-spin">
+          <AiOutlineLoading3Quarters />
+        </div>
       ) : (
-        <>Cultivation {id}</>
+        <></>
       )}
     </PageWrapper>
   );
