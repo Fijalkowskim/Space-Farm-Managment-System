9fb83635b5d0834f712acf9aeab173cdd3d6a08a
Mbossowski <92019363+Mbossowski@users.noreply.github.com>
2024-05-15 17:06:34 +0200

    Added user permissions verification for updateCultivation method including the people responsible for the cultivation

--- backend/src/main/java/com/Fijalkowskim/SpaceFarmManagmentSystem/repositories/CultivationDAORepository.java
+++ backend/src/main/java/com/Fijalkowskim/SpaceFarmManagmentSystem/repositories/CultivationDAORepository.java
@@ -1,7 +1,6 @@
 package com.Fijalkowskim.SpaceFarmManagmentSystem.repositories;
 
 import com.Fijalkowskim.SpaceFarmManagmentSystem.models.Cultivation;
-import com.Fijalkowskim.SpaceFarmManagmentSystem.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
