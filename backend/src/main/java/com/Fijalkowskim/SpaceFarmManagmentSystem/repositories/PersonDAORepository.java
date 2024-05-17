568d12c7fae972d13bc0212ec78d6de51ec9f05f
Paweł <pl300615@student.polsl.pl>
2024-05-17 19:46:09 +0200

    Login function added to PersonController

--- backend/src/main/java/com/Fijalkowskim/SpaceFarmManagmentSystem/repositories/PersonDAORepository.java
+++ backend/src/main/java/com/Fijalkowskim/SpaceFarmManagmentSystem/repositories/PersonDAORepository.java
@@ -2,8 +2,12 @@ package com.Fijalkowskim.SpaceFarmManagmentSystem.repositories;
 
 import com.Fijalkowskim.SpaceFarmManagmentSystem.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.Set;
 
 public interface PersonDAORepository extends JpaRepository<Person, Long> {
     Set<Person> findAllByCultivations_Id(long cultivationId);
+
+    Optional<Person> findByLogin(String login);
 }
