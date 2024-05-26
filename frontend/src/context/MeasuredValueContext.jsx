9b0c1755bb7d4cb2b36117c736221efa43a8da07
Paweł Ledwoń <pl300615@student.polsl.pl>
2024-05-26 14:53:59 +0200

    MeasuredValueContext updated

--- frontend/src/context/MeasuredValueContext.jsx
+++ frontend/src/context/MeasuredValueContext.jsx
@@ -10,32 +10,77 @@ export function useMeasuredValueContext() {
 export function MeasuredValueContextProvider({ children }) {
   //************ Get methods ************
   const getMeasuredValues = async () => {
-    return;
     try {
       const res = await api.get("/measuredValue");
       if (res.data) {
+        return res.data.content; 
       }
     } catch (err) {
       console.log(err);
     }
+    return [];
   };
-  const getMeasuredValue = async (id) => {
-    return;
-    //return exampleMeasuredValues.find((MeasuredValue) => MeasuredValue.id === id);
 
+  const getMeasuredValue = async (id) => {
     try {
       const res = await api.get(`/measuredValue/${id}`);
       if (res.data) {
+        return res.data;
       }
     } catch (err) {
       console.log(err);
     }
+    return null;
   };
+
+  const addMeasuredValue = async (measuredValueRequest) => {
+    try {
+      const res = await api.put("/measuredValue", measuredValueRequest);
+      if (res.data) {
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+
+  const updateMeasuredValue = async (id, measuredValueRequest) => {
+    try {
+      const res = await api.post(`/measuredValue/${id}`, measuredValueRequest,{
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+
+  const deleteMeasuredValue = async (id) => {
+    try {
+      const res = await api.delete(`/measuredValue/${id}`);
+      if (res.status === 200) {
+        return true;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
+  };
+
   return (
     <MeasuredValueContext.Provider
       value={{
         getMeasuredValues,
         getMeasuredValue,
+        addMeasuredValue,
+        updateMeasuredValue,
+        deleteMeasuredValue,
       }}
     >
       {children}
