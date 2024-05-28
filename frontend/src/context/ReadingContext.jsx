d5d9403b984f3cf6f84a8afa633eb84c4c6912ed
Adam Łukaszewicz <jakub.adam20@gmail.com>
2024-05-28 14:58:24 +0200

    ReadingContext updated

--- frontend/src/context/ReadingContext.jsx
+++ frontend/src/context/ReadingContext.jsx
@@ -8,34 +8,93 @@ export function useReadingContext() {
 }
 
 export function ReadingContextProvider({ children }) {
+  const [readings, setReadings] = useState([]);
+  const [reading, setReading] = useState(null);
+
   //************ Get methods ************
   const getReadings = async () => {
-    return;
     try {
       const res = await api.get("/reading");
       if (res.data) {
+        setReadings(res.data.content);
+        return res.data.content;
       }
     } catch (err) {
       console.log(err);
     }
+    return undefined;
   };
   const getReading = async (id) => {
-    return;
     //return exampleReadings.find((Reading) => Reading.id === id);
 
     try {
       const res = await api.get(`/reading/${id}`);
       if (res.data) {
+        setReading(res.data);
+        return res.data;
       }
     } catch (err) {
       console.log(err);
     }
+    return undefined;
+  };
+  //************ Put methods ************
+  const addReading = async (readingRequest) => {
+    try {
+      const res = await api.put("/reading", readingRequest, {
+        headers:{
+          "Content-Type": "application/json",
+        }
+      });
+      if (res.data) {
+        setReadings((prevReadings) => [...prevReadings, res.data]);
+        return res.data
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+  //************ Post methods ************
+  const updateReading = async (id, readingRequest) => {
+    try {
+      const res = await api.post(`/reading/${id}`, readingRequest, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        setReadings((prevReadings) =>
+            prevReadings.map((reading) => (reading.id === id ? res.data : reading))
+        );
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+  //************ Delete methods ************
+  const deleteReading = async (id) => {
+    try {
+      await api.delete(`/reading/${id}`);
+      setReadings((prevReadings) => prevReadings.filter((reading) => reading.id !== id));
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
   };
   return (
     <ReadingContext.Provider
       value={{
+        reading,
+        readings,
         getReadings,
         getReading,
+        addReading,
+        updateReading,
+        deleteReading,
       }}
     >
       {children}
