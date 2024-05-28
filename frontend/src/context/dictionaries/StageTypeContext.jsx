7107ec8a08aaca8fbd24b8f2360ebfbcdf3cce6b
Adam Łukaszewicz <jakub.adam20@gmail.com>
2024-05-28 16:21:22 +0200

    Update StageTypeContext.jsx

--- frontend/src/context/dictionaries/StageTypeContext.jsx
+++ frontend/src/context/dictionaries/StageTypeContext.jsx
@@ -8,34 +8,92 @@ export function useStageTypeContext() {
 }
 
 export function StageTypeContextProvider({ children }) {
+  const [stageTypes, setStageTypes] = useState([]);
+  const [stageType, setStageType] = useState(null);
   //************ Get methods ************
   const getStageTypes = async () => {
-    return;
     try {
-      const res = await api.get("/stageType");
+      const res = await api.get("/dictionaries/stage-types/");
       if (res.data) {
+        setStageTypes(res.data.content);
+        return res.data.content;
       }
     } catch (err) {
       console.log(err);
     }
+    return null;
   };
   const getStageType = async (id) => {
-    return;
     //return exampleStageTypes.find((StageType) => StageType.id === id);
 
     try {
-      const res = await api.get(`/stageType/${id}`);
+      const res = await api.get(`/dictionaries/stage-types/${id}`);
       if (res.data) {
+        setStageType(res.data);
+        return res.data;
       }
     } catch (err) {
       console.log(err);
     }
+    return null;
+  };
+  //************ Put methods ************
+  const addStageType = async (stageTypeRequest) => {
+    try {
+      const res = await api.put("/dictionaries/stage-types/", stageTypeRequest, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        setStageTypes((prevStageTypes) => [...prevStageTypes, res.data]);
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+  //************ Post methods ************
+  const updateStageType = async (id, stageTypeRequest) => {
+    try {
+      const res = await api.post(`/dictionaries/stage-types/${id}`, stageTypeRequest, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        setStageTypes((prevStageTypes) =>
+            prevStageTypes.map((stageType) => (stageType.id === id ? res.data : stageType))
+        );
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+  //************ Delete methods ************
+  const deleteStageType = async (id) => {
+    try {
+      await api.delete(`/dictionaries/stage-types/${id}`);
+      setStageTypes((prevStageTypes) => prevStageTypes.filter((stageType) => stageType.id !== id));
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
   };
   return (
     <StageTypeContext.Provider
       value={{
+        stageType,
+        stageTypes,
         getStageTypes,
         getStageType,
+        addStageType,
+        updateStageType,
+        deleteStageType,
       }}
     >
       {children}
