cd13bb9e158205137ca5f161e32521fc8566388f
Paweł Ledwoń <pl300615@student.polsl.pl>
2024-06-02 12:15:22 +0200

    Stage context

--- frontend/src/context/StageContext.jsx
+++ frontend/src/context/StageContext.jsx
@@ -9,29 +9,95 @@ export function useStageContext() {
 }
 
 export function StageContextProvider({ children }) {
+  const [stages, setStages] = useState(exampleStages);
+  const [stage, setStage] = useState(null);
+
   //************ Get methods ************
   const getStages = async () => {
+    try {
+      const res = await api.get("/stage");
+      if (res.data) {
+        setStages(res.data.content); // Assuming res.data.content contains the list of stages
+        return res.data.content;
+      }
+    } catch (err) {
+      console.log(err);
+    }
     return exampleStages;
-    try {
-      const res = await api.get("/plants");
-      if (res.data) {
-      }
-    } catch (err) {
-      console.log(err);
-    }
   };
+
   const getStage = async (id) => {
-    return exampleStages.find((cultivation) => cultivation.id === id);
     try {
-      const res = await api.get(`/plant/${id}`);
+      const res = await api.get(`/stage/${id}`);
       if (res.data) {
+        setStage(res.data);
+        return res.data;
       }
     } catch (err) {
       console.log(err);
     }
+    return exampleStages.find((stage) => stage.id === id);
   };
+
+  const addStage = async (stageRequest) => {
+    try {
+      const res = await api.put("/stage", stageRequest, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        setStages((prevStages) => [...prevStages, res.data]);
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+
+  const updateStage = async (id, stageRequest) => {
+    try {
+      const res = await api.post(`/stage/${id}`, stageRequest, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.data) {
+        setStages((prevStages) =>
+          prevStages.map((stage) => (stage.id === id ? res.data : stage))
+        );
+        return res.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  };
+
+  const deleteStage = async (id) => {
+    try {
+      await api.delete(`/stage/${id}`);
+      setStages((prevStages) => prevStages.filter((stage) => stage.id !== id));
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
+  };
+
   return (
-    <StageContext.Provider value={{ getStages, getStage }}>
+    <StageContext.Provider
+      value={{
+        stages,
+        stage,
+        getStages,
+        getStage,
+        addStage,
+        updateStage,
+        deleteStage,
+      }}
+    >
       {children}
     </StageContext.Provider>
   );
