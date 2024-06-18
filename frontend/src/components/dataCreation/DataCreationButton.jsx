94397a3c1d1ae0273381cd5e07686abab1d0d1f1
Mateusz <fijalkowskim01@gmail.com>
2024-06-18 20:16:56 +0200

    Station creation

--- frontend/src/components/dataCreation/DataCreationButton.jsx
+++ frontend/src/components/dataCreation/DataCreationButton.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import CustomButton from "../general/CustomButton";
 import { useDataCreationContext } from "../../context/general/DataCreationContext";
+import { useStationContext } from "../../context/StationContext";
+import { useGlobalReloadContext } from "../../context/general/GlobalReloadContext";
 
 function DataCreationButton({ dataType, argumentsFromParent }) {
   const { startCreatingObjectByType } = useDataCreationContext();
+  const { addStation } = useStationContext();
+  const { setGlobalReload } = useGlobalReloadContext();
   return (
     <button className={"w-full"}>
       <CustomButton
         className={"w-full text-base"}
-        onClick={() => startCreatingObjectByType(dataType, argumentsFromParent)}
+        onClick={() => {
+          if (dataType === "station") {
+            addStation();
+            setGlobalReload(true);
+          } else startCreatingObjectByType(dataType, argumentsFromParent);
+        }}
       >
         Create new
       </CustomButton>
